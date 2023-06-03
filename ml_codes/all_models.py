1027d691e5843ef69257f509d30faf978755d8fd
Sakshi Rathi <sakshi.m.rathi777@gmail.com>
2023-06-04 00:20:30 +0530

    Delete all_models.py

--- ml_codes/all_models.py
+++
@@ -1,3 +0,0 @@
-def maain():
-    print("Instead of checknig this repository go to google colud use feature called model garden and direcly ope nthem in google colab without understanding them and you can see the results......")
-main()
